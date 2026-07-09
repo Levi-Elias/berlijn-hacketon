59f8b80922bda2ef2cb378717b5e65a9a27e9ab4
Levi-Elias <le.sastro@outlook.com>
2026-07-09 23:22:15 +0200

    sql

--- berlijn_hackathon.sql
+++ berlijn_hackathon.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `berlijn_hackathon`
+-- Database: `berlijn-hackathon`
 --
 
 -- --------------------------------------------------------
